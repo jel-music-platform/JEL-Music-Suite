7997000d1302308a58bb64365cb7d8cda38af3e9
Juan <reformaselipe@gmail.com>
2026-07-29 21:44:47 +0200

    BUILD-0004 CORE knowledge model - Add MusicalDNA value object

---
+++ src/JELMusic.Domain/ValueObjects/MusicalDNA.cs
@@ -0,0 +1,36 @@
+namespace JELMusic.Domain.ValueObjects;
+
+public sealed class MusicalDNA
+{
+    public string Influences { get; private set; }
+
+    public string Instruments { get; private set; }
+
+    public string Mood { get; private set; }
+
+    public int TempoBpm { get; private set; }
+
+    public string VocalStyle { get; private set; }
+
+    private MusicalDNA()
+    {
+        Influences = string.Empty;
+        Instruments = string.Empty;
+        Mood = string.Empty;
+        VocalStyle = string.Empty;
+    }
+
+    public MusicalDNA(
+        string influences,
+        string instruments,
+        string mood,
+        int tempoBpm,
+        string vocalStyle)
+    {
+        Influences = influences;
+        Instruments = instruments;
+        Mood = mood;
+        TempoBpm = tempoBpm;
+        VocalStyle = vocalStyle;
+    }
+}
