a5e13349bb8ea2ff9a649f8137e15b7e6b0b28ac
Juan <reformaselipe@gmail.com>
2026-07-29 21:35:44 +0200

    BUILD-0003 Domain vocabulary - Add musical classification enums

---
+++ src/JELMusic.Domain/Enums/CreationType.cs
@@ -0,0 +1,14 @@
+namespace JELMusic.Domain.Enums;
+
+public enum CreationType
+{
+    Unknown = 0,
+
+    Song = 1,
+
+    Instrumental = 2,
+
+    Soundtrack = 3,
+
+    Arrangement = 4
+}
---
+++ src/JELMusic.Domain/Enums/MusicalGenre.cs
@@ -0,0 +1,22 @@
+namespace JELMusic.Domain.Enums;
+
+public enum MusicalGenre
+{
+    Unknown = 0,
+
+    Pop = 1,
+
+    Folk = 2,
+
+    Worship = 3,
+
+    Gospel = 4,
+
+    Cinematic = 5,
+
+    Classical = 6,
+
+    Electronic = 7,
+
+    Fusion = 8
+}
