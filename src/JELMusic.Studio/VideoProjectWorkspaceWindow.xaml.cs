9558892b5a767e09c85e0692cefffc9db0b2ad75
Juan <reformaselipe@gmail.com>
2026-09-19 11:23:32 +0200

    BUILD-0058 Add dynamic video timeline

--- src/JELMusic.Studio/VideoProjectWorkspaceWindow.xaml.cs
+++ src/JELMusic.Studio/VideoProjectWorkspaceWindow.xaml.cs
@@ -1,4 +1,6 @@
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using JELMusic.Application.Abstractions.Dispatching;
 using JELMusic.Application.Queries.GetVideoProjectById;
 
@@ -37,6 +39,8 @@ public partial class VideoProjectWorkspaceWindow : Window
                 StatusTextBlock.Text = result.Status.ToString();
                 ConceptTextBlock.Text = result.Concept;
                 DurationTextBlock.Text = result.Duration.ToString(@"mm\:ss");
+
+                BuildTimeline(result.Duration);
             }
             catch (Exception ex)
             {
@@ -49,5 +53,72 @@ public partial class VideoProjectWorkspaceWindow : Window
                 DialogResult = false;
             }
         };
+    }
+
+    private void BuildTimeline(TimeSpan duration)
+    {
+        TimelineRuler.Children.Clear();
+
+        TimelineDurationTextBlock.Text =
+            duration.ToString(@"mm\:ss");
+
+        TimelineRuler.SizeChanged += (_, _) =>
+        {
+            DrawTimelineRuler(duration);
+        };
+    }
+
+    private void DrawTimelineRuler(TimeSpan duration)
+    {
+        TimelineRuler.Children.Clear();
+
+        if (duration.TotalSeconds <= 0 ||
+            TimelineRuler.ActualWidth <= 0)
+        {
+            return;
+        }
+
+        var totalSeconds = duration.TotalSeconds;
+        var width = TimelineRuler.ActualWidth;
+
+        var interval = totalSeconds <= 60
+            ? 10
+            : 30;
+
+        for (var seconds = 0.0;
+             seconds <= totalSeconds;
+             seconds += interval)
+        {
+            var position = seconds / totalSeconds * width;
+
+            var marker = new Border
+            {
+                Width = 1,
+                Height = 8,
+                Background = new SolidColorBrush(
+                    Color.FromRgb(100, 100, 100)),
+                HorizontalAlignment = HorizontalAlignment.Left
+            };
+
+            Canvas.SetLeft(marker, position);
+            Canvas.SetTop(marker, 0);
+
+            TimelineRuler.Children.Add(marker);
+
+            var label = new TextBlock
+            {
+                Text = TimeSpan
+                    .FromSeconds(seconds)
+                    .ToString(@"mm\:ss"),
+                FontSize = 10,
+                Foreground = new SolidColorBrush(
+                    Color.FromRgb(120, 120, 120))
+            };
+
+            Canvas.SetLeft(label, position + 4);
+            Canvas.SetTop(label, 8);
+
+            TimelineRuler.Children.Add(label);
+        }
     }
 }
